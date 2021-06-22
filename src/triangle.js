61ad109f52f44427296355efba54ad02b0df0f68
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-21 22:27:34 -0700

    test whether a triangle is scalene

--- src/triangle.js
+++ src/triangle.js
@@ -3,3 +3,11 @@ export default function Triangle(side1, side2, side3) {
   this.side2 = side2;
   this.side3 = side3;
 }
+
+Triangle.prototype.checkType = function() {
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+    return "not a triangle";
+  } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+    return "scalene triangle";
+  }
+};
